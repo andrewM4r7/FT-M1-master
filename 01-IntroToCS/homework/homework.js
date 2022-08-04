ffbc3f07055cf1e34f13acebb0f62a2a345e8039
andrewM4r7 <atienzaandres7@gmail.com>
2022-08-04 18:29:55 -0500

    homework CS

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,23 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let sum = 0;
 
-}
+  for (let i = 0; i < num.length; i++) {
+     sum += +num[i] * 2 ** (num.length - 1 - i);
+  }
+  return sum;
+
+  }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+let result = [];
+while (num > 0) {
+  result.push(num%2);
+  num = Math.floor(num/2);
+}
+return result.reverse().join('');
 }
 
 
